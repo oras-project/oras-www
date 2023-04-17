7e968314ffa4eae4a2ffbae42e596226a9955935
Asmit Malakannawar <asmitbm2952002@gmail.com>
2023-04-17 19:37:27 +0530

    feat: add anouncement bar and features content
    Signed-off-by: Asmit Malakannawar <asmitbm2952002@gmail.com>

--- src/components/Features/index.jsx
+++ src/components/Features/index.jsx
@@ -4,27 +4,27 @@ import styles from './styles.module.css';
 const features = [
     {
         title: 'Artifact Reference',
-        content: (
-            <>
-                Able to attach supply chain artifacts to container images and show the artifact reference relationship.
-            </>
-        ),
+        content: [
+            "Attach supply chain artifacts to container images.",
+            "Discover and show the artifact reference relations.",
+            "Extend the registries not just for storing container images.",
+        ],
     },
     {
         title: 'Distributed Software Artifacts',
-        content: (
-            <>
-                Pull, push, and move any software artifacts across cloud and on-prem OCI registries.
-            </>
-        ),
+        content: [
+            "Manage artifacts in OCI registries.",
+            "Migrate artifacts across registries.",
+            "Manage artifacts in file system through OCI image layout.",
+        ],
     },
     {
-        title: 'Manage Registry and Repository',
-        content: (
-            <>
-                Able to manage tags, repositories in OCI registry with fine-grained capabilities.
-            </>
-        ),
+        title: 'Explore and Manage OCI Image',
+        content: [
+            "Manage image manifest and layer in an OCI registry.",
+            "Operate tag and repository in an OCI registry.",
+            "Explore the detailed content of an OCI image.",
+        ],
     },
 ];
 
@@ -38,7 +38,15 @@ export default function Features() {
                 <div className={styles.feature_card} key={index}>
                     <div />
                     <h3>{title}</h3>
-                    <p className={styles.info}>{content}</p>
+
+                    <ul className={styles.list}>
+                        {content.map((use, index) => (
+                            <li key={index} className={styles.listItem}>
+                                {use}
+                            </li>
+                        ))}
+                    </ul>
+
                 </div>
             ))}
         </div>
