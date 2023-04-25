090ba5e75c9873fe459e99aee88c6a6efbb6109c
Asmit Malakannawar <asmitbm2952002@gmail.com>
2023-04-25 21:49:40 +0530

    fix redirect issues
    Signed-off-by: Asmit Malakannawar <asmitbm2952002@gmail.com>

---
+++ src/pages/oras-go/v2/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import Head from '@docusaurus/Head';
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={siteConfig.title}
+      description="Description will go into a meta tag in <head />">
+        <Head>
+        <meta charset="utf-8"/>
+        <meta name="go-import" content="oras.land/oras-go/v2 git https://github.com/oras-project/oras-go"/>
+        <meta name="go-source" content="oras.land/oras-go/v2 git https://github.com/oras-project/oras-go https://github.com/oras-project/oras-go/tree/main{/dir} https://github.com/oras-project/oras-go/blob/main{/dir}/{file}#L{line}"/>
+        <meta http-equiv="refresh" content="0; url=https://github.com/oras-project/oras-go"/>
+      </Head>
+    </Layout>
+  );
+}
---
+++ src/pages/oras/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import Head from '@docusaurus/Head';
+
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+  return (
+    <Layout
+      title={siteConfig.title}
+      description="Description will go into a meta tag in <head />">
+        <Head>
+        <meta name="go-import" content="oras.land/oras git https://github.com/oras-project/oras"/>
+        <meta name="go-source" content="oras.land/oras git https://github.com/oras-project/oras https://github.com/oras-project/oras/tree/main{/dir} https://github.com/oras-project/oras/blob/main{/dir}/{file}#L{line}"/>
+        <meta http-equiv="refresh" content="0; url=https://github.com/oras-project/oras"/>
+      </Head>
+    </Layout>
+  );
+}
