df333d3565412f71b73ebc5a8952616a0b050e2b
Feynman Zhou <feynmanzhou@microsoft.com>
2023-09-06 14:05:50 +0800

    update site version to v1.1.0
    Signed-off-by: Feynman Zhou <feynmanzhou@microsoft.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
           lastVersion: 'current',
           versions: {
             current: {
-              label: '1.0.0',
+              label: '1.1.0',
             },
           },
         },
