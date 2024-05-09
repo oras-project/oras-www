4e909fa4d9ee5f510bb2b1ed9eced49088524d9d
Asmit Malakannawar <81866614+asmitbm@users.noreply.github.com>
2024-05-09 11:17:16 +0530

    update x logo (#300)
    Updated X logo in community links section
    
    Prev: 
    
    ![image](https://github.com/oras-project/oras-www/assets/81866614/c2a6160f-d103-43f1-9490-078b23d3a93a)
    
    
    Current: 
    
    ![image](https://github.com/oras-project/oras-www/assets/81866614/ce096eef-2d7a-42f3-9dce-04d6fd965b95)
    
    Signed-off-by: Asmit Malakannawar <asmitbm2952002@gmail.com>

--- src/components/JoinCommunity/index.jsx
+++ src/components/JoinCommunity/index.jsx
@@ -1,20 +1,38 @@
-import React from 'react';
-import styles from './styles.module.css';
-import LinkCard from './community_links';
-import Link from '@docusaurus/Link';
+import React from "react";
+import styles from "./styles.module.css";
+import LinkCard from "./community_links";
+import Link from "@docusaurus/Link";
 
 export default function Community() {
     return (
-        <div className={[styles.community, styles.section_padding].join(' ')}>
+        <div className={[styles.community, styles.section_padding].join(" ")}>
             <div className={styles.join_community}>
                 <h1>Join the Community</h1>
-                <p>Join the ever-growing ORAS community for updates, support, and collaboration.</p>
+                <p>
+                    Join the ever-growing ORAS community for updates, support,
+                    and collaboration.
+                </p>
             </div>
             <div className={styles.community_links}>
                 <div className={styles.links}>
-                    <LinkCard link="https://cloud-native.slack.com/archives/CJ1KHJM5Z" title="Slack" light="img/community_links/slack.svg" dark="img/community_links/slack.svg" />
-                    <LinkCard link="https://github.com/oras-project" title="GitHub" light="img/community_links/github_light.svg" dark="img/community_links/github_dark.svg" />
-                    <LinkCard link="https://x.com/orasproject" title="X" light="img/community_links/x.svg" dark="img/community_links/x.svg" />
+                    <LinkCard
+                        link="https://cloud-native.slack.com/archives/CJ1KHJM5Z"
+                        title="Slack"
+                        light="img/community_links/slack.svg"
+                        dark="img/community_links/slack.svg"
+                    />
+                    <LinkCard
+                        link="https://github.com/oras-project"
+                        title="GitHub"
+                        light="img/community_links/github_light.svg"
+                        dark="img/community_links/github_dark.svg"
+                    />
+                    <LinkCard
+                        link="https://x.com/orasproject"
+                        title="X (Twitter)"
+                        light="img/community_links/x.svg"
+                        dark="img/community_links/x.svg"
+                    />
                 </div>
                 <div className={styles.add_to_calendar}>
                     <div className={styles.calendar_heading}>
@@ -22,7 +40,13 @@ export default function Community() {
                         <h4>Biweekly Tuesday of every month | 5:00 PM PDT</h4>
                     </div>
                     <div className={styles.calendar_link}>
-                        <Link class="button button--secondary button--lg" target='_blank' href="https://hackmd.io/P-O6n222TcSMoJgHmTTduw">Add to Calendar</Link>
+                        <Link
+                            class="button button--secondary button--lg"
+                            target="_blank"
+                            href="https://hackmd.io/P-O6n222TcSMoJgHmTTduw"
+                        >
+                            Add to Calendar
+                        </Link>
                     </div>
                 </div>
             </div>
