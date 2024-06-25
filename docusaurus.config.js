5d2cbe23801ebbc599a34b0c246f2493690a6bc0
Terry Howe <tlhowe@amazon.com>
2024-06-25 17:53:37 -0600

    Remove next documents in favor of two releases at a time. (#344)
    Maintaining 3 copies of the documents will be very difficult making sure
    fixes happen for each version. This PR reduces the number to 2. We can
    use the latest release at the basis for the next new release.
    
    Signed-off-by: Terry Howe <terrylhowe@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -53,6 +53,7 @@ const config = {
                     // Remove this to remove the "edit this page" links.
                     editUrl:
                         "https://github.com/oras-project/oras-www/tree/main/",
+                    includeCurrentVersion: false,
                 },
                 blog: {
                     showReadingTime: true,
