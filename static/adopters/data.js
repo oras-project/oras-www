21abfa098ce251dd98951766e405f196992160b6
Mathieu Benoit <mathieu-benoit@hotmail.fr>
2024-11-05 09:13:38 -0500

    [Add adopter]: Score (#410)
    https://github.com/oras-project/oras-www/issues/409
    
    ---------
    
    Signed-off-by: Mathieu Benoit <mathieu-benoit@hotmail.fr>

--- static/adopters/data.js
+++ static/adopters/data.js
@@ -62,6 +62,13 @@ const projects = [
         description: "A little helper that pushes Helm Charts and images to your registries, easily configured with a declarative spec.",
         link: "https://github.com/christoffernissen/helmper",
     },
+    {
+        title: "Score",
+        light: "img/adopters/score.svg",
+        dark: "img/adopters/score.svg",
+        description: "Score is a developer-centric and platform-agnostic workload specification. It ensures consistent configuration between local and remote environments.",
+        link: "https://score.dev",
+    },
 ];
 
 const registries = [
