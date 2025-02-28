be6c8eae3ccf109161035801913891cd1be28f90
Feynman Zhou <feynmanzhou@microsoft.com>
2025-02-27 16:16:42 -0800

    doc: update banner and installation page (#458)
    Updated the banner and installation page to reflect the latest changes
    in the project.
    
    ![image](https://github.com/user-attachments/assets/653df6d3-ce75-4a39-abcc-04d584db80b5)
    
    Signed-off-by: Feynman Zhou <feynmanzhou@microsoft.com>

---
+++ src/theme/DocVersionBanner/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {useActiveDocContext} from '@docusaurus/plugin-content-docs/client';
+
+export default function DocVersionBanner() {
+  const { activeVersion } = useActiveDocContext();
+  if (!activeVersion) {
+    return null;
+  }
+
+  if (activeVersion.label === '1.3.0-beta') {
+    return (
+      <div style={{ background: '#FFF4DB', padding: '10px', borderRadius: '5px' }}>
+        <strong>This is documentation for OCI Registry As Storage <b>1.3.0-beta</b>, which is in a preview status.</strong>
+        <br />
+        For a stable release documentation, see the <a href="/docs/latest">latest version (1.2)</a>.
+      </div>
+    );
+  }
+  return null;
+}
