cdecb3d8e32aac3dc08a487f999822a5e00c2f80
Billy Zha <jinzha1@microsoft.com>
2025-05-09 10:42:47 +0800

    fix: update link for latest documentations (#472)
    This pull request includes a minor update to the `DocVersionBanner`
    component. The change updates the link for the stable release
    documentation to point to the `/docs` endpoint instead of
    `/docs/latest`.
    
    Fixes #471
    
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- src/theme/DocVersionBanner/index.js
+++ src/theme/DocVersionBanner/index.js
@@ -12,7 +12,7 @@ export default function DocVersionBanner() {
       <div style={{ background: '#FFF4DB', padding: '10px', borderRadius: '5px' }}>
         <strong>This is documentation for OCI Registry As Storage <b>1.3.0-beta</b>, which is in a preview status.</strong>
         <br />
-        For a stable release documentation, see the <a href="/docs/latest">latest version (1.2)</a>.
+        For a stable release documentation, see the <a href="/docs">latest version (1.2)</a>.
       </div>
     );
   }
